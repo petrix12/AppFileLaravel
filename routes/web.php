62dbd96a07a67eae6ee91ce57c82cf3db08e6352
Pedro Jesús Bazó Canelón <bazo.pedro@gmail.com>
2021-03-22 16:30:15 -0400

    P2 Rutas de mantenimiento

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FilesController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,4 +34,43 @@ Route::get('/ver/{file}', [FilesController::class, 'ver'])->name('user.files.ver
 Route::get('/files/{file}', [FilesController::class, 'show'])->name('user.files.show');
 
 Route::delete('/delet-file/{file}', [FilesController::class, 'destroy'])->name('user.files.destroy');
+
+
+
+// RUTAS PARA EL MANTENIMIENTO DE LA APLICACIÓN EN PRODUCCIÓN
+// Ruta para ejecutar en producción: $ php artisan key:generate
+Route::get('key-generate', function(){
+    Artisan::call('key:generate');
+});
+
+// Ruta para ejecutar en producción: $ php artisan storage:link
+Route::get('storage-link', function(){
+    Artisan::call('storage:link');
+});
+
+// Ruta para ejecutar en producción: $ php artisan config:cache
+Route::get('config-cache', function(){
+    Artisan::call('config:cache');
+});
+
+// Ruta para ejecutar en producción: $ php artisan cache:clear
+Route::get('cache-clear', function(){
+    Artisan::call('cache:clear');
+});
+
+// Ruta para ejecutar en producción: $ php artisan route:clear
+Route::get('route-clear', function(){
+    Artisan::call('route:clear');
+});
+
+// Ruta para ejecutar en producción: $ php artisan config:clear
+Route::get('config-clear', function(){
+    Artisan::call('config:clear');
+});
+
+// Ruta para ejecutar en producción: $ php artisan view:clear
+Route::get('view-clear', function(){
+    Artisan::call('view:clear');
+});
+
 
